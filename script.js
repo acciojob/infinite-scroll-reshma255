1e5f789367a945b852628847f9f9f48cb6ba4867
Reshma Patil <157274743+reshma255@users.noreply.github.com>
2026-01-02 21:33:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 //your code here!
+const list = document.getElementById("list");
+let itemCount = 0;
 
+function addItems(num) { 
+	for (let i = 0; i < num; i++) { 
+		itemCount++; 
+		const li = document.createElement("li");
+		li.textContent = `Item ${itemCount}`;
+		list.appendChild(li); 
+	} 
+}
+addItems(10);
+
+window.addEventListener("scroll", () => {
+	if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+         addItems(2); 
+	} 
+});
+
+
+		
